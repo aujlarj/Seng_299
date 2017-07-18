319838775feaafb564d44500b1639efbe2080444
aujlarj <aujla.rj@hotmail.com>
2017-07-17 23:33:47 -0700

    Remove Shillbot

--- ShillBot/start_basic_worker.py
+++
@@ -1,7 +0,0 @@
-
-from workers.basic_worker import BasicUserParseWorker
-
-
-if __name__ == "__main__":
-    worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-    worker.run()
--- ShillBot/start_mothership.py
+++
@@ -1,7 +0,0 @@
-
-from mothership.base import MothershipServer
-
-
-if __name__ == "__main__":
-    server = MothershipServer()
-    server.run()
